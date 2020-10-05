746deb3471c73aee270c2aed25068503f1df8660
Jae-Eon <53307559+Jae-Eon@users.noreply.github.com>
2020-10-05 11:38:17 +0900

    Why is it tough to save and check?

---
+++ script/(20200928)Numbers.js
@@ -0,0 +1,50 @@
+function eg1() {
+  var x = 10;
+  var y = 20;
+  var z = x + y;
+  console.log(z);
+}
+
+function eg2() {
+  var x = "10";
+  var y = "20";
+  var z = x + y;
+  console.log(z)
+}
+
+function eg3() {
+  var x = 10;
+  var y = 20;
+  var z = "30";
+  var result = x + y + z;
+  console.log(result);
+}
+
+function eg4() {
+  var x = "100";
+  var y = "10";
+  var z = x / y;
+  console.log(z)
+}
+
+function eg5() {
+  var x = "100";
+  var y = "10";
+  var z = x * y;
+  console.log(z)
+
+}
+
+function eg6() {
+  var x = "100";
+  var y = "10";
+  var z = x– y;
+  console.log(z)
+}
+
+function eg7() {
+  var x = "100";
+  var y = "10";
+  var z = x + y;
+  console.log(z);
+}
